d6f5974ad150bbbf6f69dee8aa9218576824cf65
Jacques-Etienne Beaudet <jebeaudet@gmail.com>
2019-02-24 17:21:44 -0500

    changes.newValue ain't a map!

--- js/popup.js
+++ js/popup.js
@@ -30,11 +30,9 @@ function registerEvents() {
 
 function registerChromeSyncCallback(){
     chrome.storage.onChanged.addListener(function(changes, namespace) {
-        for (var key in changes) {
-            if(key === "savedRoutes"){
-                var storageChange = changes[key];
-                localStorage.setItem("savedRoutes", storageChange.newValue["savedRoutes"]);
-            }
+        var newValue = changes["savedRoutes"]
+        if(newValue !== "undefined"){
+            localStorage.setItem("savedRoutes", newValue.newValue);
         }
     });
 }
