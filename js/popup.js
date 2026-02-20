908491b0006f914ad0442199e13c865af66c678d
Jacques-Etienne Beaudet <jebeaudet@coveo.com>
2026-02-19 21:13:16 -0500

    new route add flow

--- js/popup.js
+++ js/popup.js
@@ -59,6 +59,18 @@ function registerEvents() {
       .getElementById("saveButton")
       .addEventListener("click", saveButtonAction);
     document
+      .getElementById("nextStep1Button")
+      .addEventListener("click", nextStep1Action);
+    document
+      .getElementById("nextStep2Button")
+      .addEventListener("click", nextStep2Action);
+    document
+      .getElementById("backStep2Button")
+      .addEventListener("click", backToStep1);
+    document
+      .getElementById("backStep3Button")
+      .addEventListener("click", backToStep2);
+    document
       .getElementById("busNumber")
       .addEventListener("click", resetToBlackColorInput);
     document
@@ -115,12 +127,99 @@ function resetToBlackColorInput() {
   this.style.color = "#000000";
 }
 
+async function nextStep1Action() {
+  $("#errorMessage").hide();
+  var busNumber = document.getElementById("busNumber").value;
+
+  if (busNumber < 1 || busNumber > 905) {
+    document.getElementById("busNumber").style.color = "#ff0000";
+    $("#nextStep1Button").effect("shake", { distance: 100 });
+    $("#errorMessage").show();
+    return;
+  }
+
+  var directions = await fetchDirections(busNumber);
+  if (!directions) {
+    $("#nextStep1Button").effect("shake", { distance: 100 });
+    $("#errorMessage").show();
+    return;
+  }
+
+  var select = document.getElementById("busDirection");
+  select.innerHTML = "";
+
+  var opt0 = document.createElement("option");
+  opt0.value = directions.codeDirectionPrincipale;
+  opt0.textContent = directions.descriptionDirectionPrincipale;
+  select.appendChild(opt0);
+
+  var opt1 = document.createElement("option");
+  opt1.value = directions.codeDirectionRetour;
+  opt1.textContent = directions.descriptionDirectionRetour;
+  select.appendChild(opt1);
+
+  $("#step1").hide();
+  $("#step2").show();
+}
+
+function nextStep2Action() {
+  $("#errorMessage").hide();
+  $("#step2").hide();
+  $("#step3").show();
+}
+
+function fetchDirections(busNumber) {
+  return jQuery
+    .ajax({
+      url: "https://www.rtcquebec.ca/cache/proxy/rtc",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+        method: "get",
+        baseURL: "https://api-iv.rtcquebec.ca/api/legacy",
+        url: "/Parcours_Periode",
+        params: {
+          noParcours: busNumber,
+          date: getFormatedTodayDate(),
+        },
+      }),
+    })
+    .then(function (data) {
+      if (
+        data &&
+        data.codeDirectionPrincipale !== undefined &&
+        data.codeDirectionRetour !== undefined
+      ) {
+        return data;
+      }
+      return null;
+    })
+    .catch(function () {
+      return null;
+    });
+}
+
+function backToStep1() {
+  $("#step2").hide();
+  $("#step1").show();
+  $("#errorMessage").hide();
+}
+
+function backToStep2() {
+  $("#step3").hide();
+  $("#step2").show();
+  $("#errorMessage").hide();
+}
+
 async function saveButtonAction() {
   $("#errorMessage").hide();
+  var directionSelect = document.getElementById("busDirection");
   var newSavedRoute = {
     stopCode: document.getElementById("busStopCode").value,
     busNumber: document.getElementById("busNumber").value,
-    direction: document.getElementById("busDirection").value,
+    direction: directionSelect.value,
+    directionDescription:
+      directionSelect.options[directionSelect.selectedIndex].text,
     id: crypto.randomUUID(),
   };
 
@@ -148,7 +247,10 @@ async function saveButtonAction() {
 function resetInputs() {
   $("#busNumber").val("");
   $("#busStopCode").val("");
-  $("#busDirection").val("0");
+  $("#busDirection").empty();
+  $("#step2").hide();
+  $("#step3").hide();
+  $("#step1").show();
 }
 
 function validateDuplicate(newSavedRoute) {
@@ -215,81 +317,90 @@ function refreshBusRoutesTable() {
 
   if (savedRoutes.length === 0) {
     $("#loading").hide();
+    return;
   }
 
+  var promises = [];
   for (let i = 0; i < savedRoutes.length; i++) {
-    var savedRoute = savedRoutes[i];
-    var url = getUrlFromSavedRoute(savedRoute);
-    $.get(
-      url,
-      function (data) {
-        if (data.horaires?.length > 0) {
-          var timeTd;
-          var liveImg;
-
-          var tr = document.createElement("TR");
-          var resultTable = document.getElementById("tableOutput");
-          var numberOfResult = Math.min(data.horaires.length, 3);
-
-          var numberTd = document.createElement("TD");
-          var stopTd = document.createElement("TD");
-          var directionTd = document.createElement("TD");
-          numberTd.rowSpan =
-            stopTd.rowSpan =
-            directionTd.rowSpan =
-              numberOfResult;
-
-          numberTd.appendChild(
-            document.createTextNode(data.parcours.noParcours),
-          );
-          stopTd.appendChild(document.createTextNode(data.arret.nom));
-
-          var directionText = data.parcours.descriptionDirection || "";
-          directionTd.appendChild(document.createTextNode(directionText));
-          directionTd.title = directionText;
-          directionTd.className = "direction-cell";
-
-          stopTd.title = data.arret.description;
-
-          tr.appendChild(numberTd);
-          tr.appendChild(stopTd);
-          tr.appendChild(directionTd);
-
-          let j = 0;
-          do {
-            timeTd = document.createElement("TD");
-            timeTd.style.whiteSpace = "nowrap";
-
-            timeTd.appendChild(
-              document.createTextNode(data.horaires[j].departMinutes + "m "),
-            );
-
-            liveImg = document.createElement("img");
-            if (data.horaires[j].ntr) {
-              liveImg.src = "img/live.png";
-              liveImg.title = chrome.i18n.getMessage("realtimeLabel");
-            } else {
-              liveImg.src = "img/clock.png";
-              liveImg.title = chrome.i18n.getMessage("scheduledLabel");
-            }
-            liveImg.height = 20;
-            liveImg.width = 20;
-            timeTd.appendChild(liveImg);
-            timeTd.title = data.horaires[j].depart;
-
-            tr.appendChild(timeTd);
-
-            resultTable.appendChild(tr);
-            j++;
-            tr = document.createElement("TR");
-          } while (j < numberOfResult);
-        }
-      },
-      "json",
-    ).fail(function () {
-      console.warn("Failed to fetch route data");
-    });
+    var url = getUrlFromSavedRoute(savedRoutes[i]);
+    promises.push(
+      $.get(url, "json")
+        .then(function (data) {
+          return data;
+        })
+        .catch(function () {
+          console.warn("Failed to fetch route data");
+          return null;
+        }),
+    );
   }
+
+  Promise.all(promises).then(function (results) {
+    var resultTable = document.getElementById("tableOutput");
+    for (let i = 0; i < results.length; i++) {
+      var data = results[i];
+      if (!data || !data.horaires?.length) continue;
+
+      var timeTd;
+      var liveImg;
+
+      var tr = document.createElement("TR");
+      var numberOfResult = Math.min(data.horaires.length, 3);
+
+      var numberTd = document.createElement("TD");
+      var stopTd = document.createElement("TD");
+      var directionTd = document.createElement("TD");
+      numberTd.rowSpan =
+        stopTd.rowSpan =
+        directionTd.rowSpan =
+          numberOfResult;
+
+      numberTd.appendChild(
+        document.createTextNode(data.parcours.noParcours),
+      );
+      stopTd.appendChild(document.createTextNode(data.arret.nom));
+
+      var directionText = data.parcours.descriptionDirection || "";
+      directionTd.appendChild(document.createTextNode(directionText));
+      directionTd.title = directionText;
+      directionTd.className = "direction-cell";
+
+      stopTd.title = data.arret.description;
+
+      tr.appendChild(numberTd);
+      tr.appendChild(stopTd);
+      tr.appendChild(directionTd);
+
+      let j = 0;
+      do {
+        timeTd = document.createElement("TD");
+        timeTd.style.whiteSpace = "nowrap";
+
+        timeTd.appendChild(
+          document.createTextNode(data.horaires[j].departMinutes + "m "),
+        );
+
+        liveImg = document.createElement("img");
+        if (data.horaires[j].ntr) {
+          liveImg.src = "img/live.png";
+          liveImg.title = chrome.i18n.getMessage("realtimeLabel");
+        } else {
+          liveImg.src = "img/clock.png";
+          liveImg.title = chrome.i18n.getMessage("scheduledLabel");
+        }
+        liveImg.height = 20;
+        liveImg.width = 20;
+        timeTd.appendChild(liveImg);
+        timeTd.title = data.horaires[j].depart;
+
+        tr.appendChild(timeTd);
+
+        resultTable.appendChild(tr);
+        j++;
+        tr = document.createElement("TR");
+      } while (j < numberOfResult);
+    }
+  });
 }
 
 function populateSavedRoutesTable() {
@@ -308,9 +419,11 @@ function populateSavedRoutesTable() {
 
       numberTd.appendChild(document.createTextNode(savedRoutes[i].busNumber));
       stopTd.appendChild(document.createTextNode(savedRoutes[i].stopCode));
-      directionTd.appendChild(
-        document.createTextNode(savedRoutes[i].direction),
-      );
+      var dirDesc =
+        savedRoutes[i].directionDescription || savedRoutes[i].direction;
+      directionTd.appendChild(document.createTextNode(dirDesc));
+      directionTd.title = dirDesc;
+      directionTd.className = "direction-cell";
       const imgLink = document.createElement("a");
       const deleteImg = document.createElement("img");
       deleteImg.src = "img/x.png";
